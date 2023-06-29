c7d2810479c845233e4d4e25336f424369e0017b
ReetotpolKumar <reetotpolkumar@gmail.com>
2023-06-29 18:40:18 +0530

    Header Section

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,659 @@
+/*import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import style from "./Navbar.module.css";
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav>
+      <Link className={style.title} to="/"></Link>
+      <div className={style.menu} onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul className={menuOpen ? style.open : ""}>
+        <li>
+          <NavLink className={style.Nav1}  to="/home">
+            Ipsum
+          </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <NavLink className={style.Nav2} activeClassName={style.active} to="/about">
+            Ipsum
+              </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <NavLink className={style.Nav3} activeClassName="active" to="/contact">
+            Ipsum
+              </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        
+        <li>
+          <NavLink className={style.Nav4} activeClassName={style.active} to="/Call">
+            Ipsum
+              </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;*/
+
+
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav>
+      <Link className="title" to="/"></Link>
+      <div className="menu" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink className="Nav1"  to="/home">
+            Ipsum
+          </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <NavLink className="Nav2" activeClassName="active" to="/about">
+            Ipsum
+              </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <NavLink className="Nav3" activeClassName="active" to="/contact">
+            Ipsum
+              </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        
+        <li>
+          <NavLink className="Nav4" activeClassName="active" to="/Call">
+            Ipsum
+              </NavLink>
+          <ul>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="#">link</NavLink>
+              <ul>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+                <li>
+                  <NavLink to="#">link</NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
